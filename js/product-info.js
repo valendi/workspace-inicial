3b6646ee201122d6c4b4fe59048ddaa31d9ecdbf
Tatiana Mazzeo <163008890+tatianamazzeo@users.noreply.github.com>
2024-10-18 09:05:25 -0300

    Dark mode en product-info

--- js/product-info.js
+++ js/product-info.js
@@ -216,3 +216,24 @@ if (cerrarSesionButton) {
     location.href = "login.html";
   });
 }
+
+//Dark mode
+const toggle = document.getElementById('darkModeToggle');
+const body = document.body;
+
+const darkModePreference = localStorage.getItem('darkMode');
+
+if (darkModePreference === 'enabled') {
+  body.classList.add('dark-mode');
+  toggle.checked = true; 
+}
+
+toggle.addEventListener('change', () => {
+  body.classList.toggle('dark-mode');
+
+  if (body.classList.contains('dark-mode')) {
+    localStorage.setItem('darkMode', 'enabled');
+  } else {
+    localStorage.setItem('darkMode', 'disabled');
+  }
+});
